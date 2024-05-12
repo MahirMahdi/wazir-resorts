0a93be6db1534d665dd83886da51bd2454f26459
Mahir <mahdimahir45@gmail.com>
2024-05-12 21:51:46 +0600

    added hotels and phone codes

---
+++ data/index.js
@@ -0,0 +1,568 @@
+export const hilton = {
+  name: "Hilton Al Habtoor City",
+  room_types: [
+    {
+      name: "King Executive Suite",
+      images: [
+        "/assets/Hilton Al Habtoor City/Executive King/01.jpg",
+        "/assets/Hilton Al Habtoor City/Executive King/02.jpg",
+        "/assets/Hilton Al Habtoor City/Executive King/03.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "King One-Bedroom Grand Canal",
+      images: [
+        "/assets/Hilton Al Habtoor City/Grand Canal Suite/01.jpg",
+        "/assets/Hilton Al Habtoor City/Grand Canal Suite/02.jpg",
+        "/assets/Hilton Al Habtoor City/Grand Canal Suite/03.jpg",
+        "/assets/Hilton Al Habtoor City/Grand Canal Suite/04.jpg",
+        "/assets/Hilton Al Habtoor City/Grand Canal Suite/05.jpg",
+      ],
+      discount: 35,
+    },
+    {
+      name: "King One Bedroom Panoramic View",
+      images: [
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/01.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/02.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/03.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/04.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/05.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/06.jpeg",
+        "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/07.jpeg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Junior Suite",
+      images: [
+        "/assets/Hilton Al Habtoor City/Junior King Suite/01.jpg",
+        "/assets/Hilton Al Habtoor City/Junior King Suite/02.jpg",
+        "/assets/Hilton Al Habtoor City/Junior King Suite/03.jpg",
+        "/assets/Hilton Al Habtoor City/Junior King Suite/04.jpg",
+      ],
+      discount: 20,
+    },
+  ],
+  gridImages: [
+    "/assets/Hilton Al Habtoor City/King One Bedroom Panoramic View/02.jpeg",
+    "/assets/Hilton Al Habtoor City/Grand Canal Suite/02.jpg",
+    "/assets/Hilton Al Habtoor City/Junior King Suite/04.jpg",
+  ],
+  subheadline:
+    "Situated off the vibrant Sheikh Zayed Road, Hilton Al Habtoor City offers guests an upscale location to escape to.",
+  benefits: [
+    "Babiole – Restaurant",
+    "Ribs & Brews – Restaurant",
+    "The Market – Buffet",
+    "Firefly – Pool Bar",
+    "The Café",
+  ],
+  logo: "/assets/hilton.png",
+  link: "/hilton-al-habtoor-city",
+};
+
+export const theMirage = {
+  name: "The Mirage",
+  room_types: [
+    {
+      name: "Executive Suite",
+      images: [
+        "/assets/The Mirage/Executive Suite/01.jpg",
+        "/assets/The Mirage/Executive Suite/02.jpg",
+        "/assets/The Mirage/Executive Suite/03.jpg",
+        "/assets/The Mirage/Executive Suite/04.jpg",
+        "/assets/The Mirage/Executive Suite/05.jpg",
+        "/assets/The Mirage/Executive Suite/06.jpg",
+        "/assets/The Mirage/Executive Suite/07.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Royal Suite",
+      images: [
+        "/assets/The Mirage/Royal Suite/03.jpg",
+        "/assets/The Mirage/Royal Suite/01.jpg",
+        "/assets/The Mirage/Royal Suite/02.jpg",
+        "/assets/The Mirage/Royal Suite/04.jpg",
+        "/assets/The Mirage/Royal Suite/05.jpg",
+        "/assets/The Mirage/Royal Suite/06.jpg",
+        "/assets/The Mirage/Royal Suite/07.jpg",
+        "/assets/The Mirage/Royal Suite/08.jpg",
+        "/assets/The Mirage/Royal Suite/09.jpg",
+        "/assets/The Mirage/Royal Suite/10.jpg",
+      ],
+      discount: 35,
+    },
+    {
+      name: "Four-Bedroom Villa",
+      images: [
+        "/assets/The Mirage/Four-Bedroom Villa/01.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/02.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/03.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/04.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/05.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/06.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/07.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/08.jpg",
+        "/assets/The Mirage/Four-Bedroom Villa/09.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Junior Suite",
+      images: [
+        "/assets/The Mirage/Junior Suite/01.jpg",
+        "/assets/The Mirage/Junior Suite/02.jpg",
+        "/assets/The Mirage/Junior Suite/03.jpg",
+        "/assets/The Mirage/Junior Suite/04.jpg",
+        "/assets/The Mirage/Junior Suite/05.jpg",
+        "/assets/The Mirage/Junior Suite/06.jpg",
+        "/assets/The Mirage/Junior Suite/07.jpg",
+      ],
+      discount: 30,
+    },
+  ],
+  gridImages: [
+    "/assets/The Mirage/Junior Suite/03.jpg",
+    "/assets/The Mirage/Four-Bedroom Villa/04.jpg",
+    "/assets/The Mirage/Royal Suite/01.jpg",
+  ],
+  subheadline:
+    "Equestrian-inspired serene level of sophistication to a world-world class city.",
+  benefits: [
+    "The Grill Pit – Restaurant",
+    "Andalucia – Buffet",
+    "Oasis Pool & Bar – Bar",
+    "Equestrian Lounge – Bar",
+    "Horse & Hound – Bar",
+    "Polo Bar – Bar",
+  ],
+  logo: "/assets/mirage.png",
+  link: "/the-mirage",
+};
+
+export const metropolitanHotel = {
+  name: "Metropolitan Hotel",
+  room_types: [
+    {
+      name: "Ambassador Suite",
+      images: [
+        "/assets/Metropolitan Hotel/Ambassador Suite/01.jpg",
+        "/assets/Metropolitan Hotel/Ambassador Suite/02.jpg",
+        "/assets/Metropolitan Hotel/Ambassador Suite/03.jpg",
+        "/assets/Metropolitan Hotel/Ambassador Suite/04.jpg",
+      ],
+      discount: 20,
+    },
+    {
+      name: "Deluxe Room",
+      images: ["/assets/Metropolitan Hotel/Deluxe Room/01.jpg"],
+      discount: 20,
+    },
+    {
+      name: "Deluxe Suite",
+      images: [
+        "/assets/Metropolitan Hotel/Deluxe Suite/01.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/02.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/03.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/04.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/05.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/06.jpg",
+        "/assets/Metropolitan Hotel/Deluxe Suite/07.jpg",
+      ],
+      discount: 20,
+    },
+    {
+      name: "Family Suite",
+      images: [
+        "/assets/Metropolitan Hotel/Family Suite/01.jpg",
+        "/assets/Metropolitan Hotel/Family Suite/02.jpg",
+        "/assets/Metropolitan Hotel/Family Suite/03.jpg",
+        "/assets/Metropolitan Hotel/Family Suite/04.jpg",
+      ],
+      discount: 20,
+    },
+  ],
+  gridImages: [
+    "/assets/Metropolitan Hotel/Ambassador Suite/02.jpg",
+    "/assets/Metropolitan Hotel/Deluxe Suite/03.jpg",
+    "/assets/Metropolitan Hotel/Family Suite/02.jpg",
+  ],
+  subheadline: "A Hotel with History.",
+  benefits: [
+    "Al Safa – Buffet",
+    "Al Sheif – Bar",
+    "The Red Lion – Bar",
+    "Tropicana – Bar",
+  ],
+  logo: "/assets/metropolitan-hotel.png",
+  link: "/metropolitan-hotel",
+};
+
+export const habtoorPalace = {
+  name: "Habtoor Palace",
+  room_types: [
+    {
+      name: "Ambassador Suite",
+      images: [
+        "/assets/Habtoor Palace/Ambassador Suite/02.jpg",
+        "/assets/Habtoor Palace/Ambassador Suite/01.png",
+        "/assets/Habtoor Palace/Ambassador Suite/03.jpg",
+      ],
+      discount: 35,
+    },
+    {
+      name: "Duchesse Suite",
+      images: [
+        "/assets/Habtoor Palace/Duchesse Suite/02.jpg",
+        "/assets/Habtoor Palace/Duchesse Suite/01.png",
+        "/assets/Habtoor Palace/Duchesse Suite/03.jpg",
+        "/assets/Habtoor Palace/Duchesse Suite/04.jpg",
+      ],
+      discount: 35,
+    },
+    {
+      name: "Empire Suite",
+      images: [
+        "/assets/Habtoor Palace/Empire Suite/02.jpg",
+        "/assets/Habtoor Palace/Empire Suite/01.png",
+        "/assets/Habtoor Palace/Empire Suite/03.jpg",
+        "/assets/Habtoor Palace/Empire Suite/04.jpg",
+      ],
+      discount: 35,
+    },
+    {
+      name: "Grand Suite",
+      images: [
+        "/assets/Habtoor Palace/Grand Suite/01.jpg",
+        "/assets/Habtoor Palace/Grand Suite/02.jpg",
+        "/assets/Habtoor Palace/Grand Suite/03.jpg",
+        "/assets/Habtoor Palace/Grand Suite/04.jpg",
+        "/assets/Habtoor Palace/Grand Suite/05.jpg",
+      ],
+      discount: 40,
+    },
+    {
+      name: "Royal Suite",
+      images: [
+        "/assets/Habtoor Palace/Royal Suite/01.jpg",
+        "/assets/Habtoor Palace/Royal Suite/02.jpg",
+        "/assets/Habtoor Palace/Royal Suite/03.jpg",
+        "/assets/Habtoor Palace/Royal Suite/04.jpg",
+      ],
+      discount: 45,
+    },
+  ],
+  gridImages: [
+    "/assets/Habtoor Palace/Grand Suite/02.jpg",
+    "/assets/Habtoor Palace/Empire Suite/02.jpg",
+    "/assets/Habtoor Palace/Duchesse Suite/02.jpg",
+  ],
+  subheadline:
+    "Habtoor Palace, LXR Hotels & Resorts is situated on the Dubai Water Canal, offering an elegant address with a waterfront theater, boutiques, stylish cafes, and signature restaurants.",
+  benefits: [
+    "Grand Grill – Restaurant (Reservation Required)",
+    "Al Dhiyafa Grand Kitchen – Buffet",
+    "Al Manara – Bar",
+    "Acaia Lounge – Bar",
+    "Polo Bar – Bar",
+    "The Underground Pub – Bar",
+    "Pool Bar – Bar",
+    "Andrea’s Beach Club – Beach Club (Reservation Required)",
+  ],
+  logo: "/assets/habtoor-palace.png",
+  link: "/habtoor-palace",
+};
+
+export const vHotel = {
+  name: "V Hotel",
+  room_types: [
+    {
+      name: "Deluxe Suite",
+      images: [
+        "/assets/V Hotel/Deluxe/01.jpg",
+        "/assets/V Hotel/Deluxe/02.jpg",
+        "/assets/V Hotel/Deluxe/03.jpg",
+      ],
+      discount: 20,
+    },
+    {
+      name: "Grand Deluxe Suite",
+      images: [
+        "/assets/V Hotel/Grand Deluxe/01.jpg",
+        "/assets/V Hotel/Grand Deluxe/02.jpg",
+        "/assets/V Hotel/Grand Deluxe/03.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Penthouse Suite",
+      images: [
+        "/assets/V Hotel/Penthouse/01.jpg",
+        "/assets/V Hotel/Penthouse/02.jpg",
+        "/assets/V Hotel/Penthouse/03.jpg",
+        "/assets/V Hotel/Penthouse/04.jpg",
+        "/assets/V Hotel/Penthouse/05.jpg",
+        "/assets/V Hotel/Penthouse/06.jpg",
+        "/assets/V Hotel/Penthouse/07.jpg",
+        "/assets/V Hotel/Penthouse/08.jpg",
+        "/assets/V Hotel/Penthouse/09.jpg",
+      ],
+      discount: 40,
+    },
+    {
+      name: "Mega Suite",
+      images: [
+        "/assets/V Hotel/Mega/01.jpg",
+        "/assets/V Hotel/Mega/02.jpg",
+        "/assets/V Hotel/Mega/03.jpg",
+        "/assets/V Hotel/Mega/04.jpg",
+        "/assets/V Hotel/Mega/05.jpg",
+        "/assets/V Hotel/Mega/06.jpg",
+      ],
+      discount: 40,
+    },
+    {
+      name: "Vibrant Suite",
+      images: [
+        "/assets/V Hotel/Vibrant/04.jpg",
+        "/assets/V Hotel/Vibrant/03.jpg",
+        "/assets/V Hotel/Vibrant/01.jpg",
+        "/assets/V Hotel/Vibrant/02.jpg",
+        "/assets/V Hotel/Vibrant/05.jpg",
+      ],
+      discount: 40,
+    },
+    {
+      name: "Valor Suite",
+      images: [
+        "/assets/V Hotel/Valor/01.jpg",
+        "/assets/V Hotel/Valor/02.jpg",
+        "/assets/V Hotel/Valor/03.jpg",
+        "/assets/V Hotel/Valor/04.jpg",
+        "/assets/V Hotel/Valor/05.jpg",
+        "/assets/V Hotel/Valor/06.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Vantage Suite",
+      images: [
+        "/assets/V Hotel/Vantage/01.jpg",
+        "/assets/V Hotel/Vantage/02.jpg",
+        "/assets/V Hotel/Vantage/03.jpg",
+        "/assets/V Hotel/Vantage/04.jpg",
+        "/assets/V Hotel/Vantage/05.jpg",
+        "/assets/V Hotel/Vantage/06.jpg",
+      ],
+      discount: 30,
+    },
+  ],
+  gridImages: [
+    "/assets/V Hotel/Vantage/02.jpg",
+    "/assets/V Hotel/Mega/02.jpg",
+    "/assets/V Hotel/Penthouse/03.jpg",
+  ],
+  subheadline:
+    "A striking balance of trend and location, step into a place styled with clear-cut sophistication at V Hotel, A Curio Collection By Hilton.",
+  benefits: [
+    "The Atrium: The Rose & Crown – English pub",
+    "Level Seven – Buffet",
+    "Sunset Lounge by Lifestyle – Bar",
+    "V Lounge – Bar",
+    "V Deck – Pool Bar",
+  ],
+  logo: "/assets/v-hotel.png",
+  link: "/v-hotel",
+};
+
+export const habtoorGrand = {
+  name: "Habtoor Grand",
+  room_types: [
+    {
+      name: "Club Oceanfront King",
+      images: [
+        "/assets/Habtoor Grand/Club Oceanfront King/01.jpeg",
+        "/assets/Habtoor Grand/Club Oceanfront King/02.jpeg",
+        "/assets/Habtoor Grand/Club Oceanfront King/03.jpeg",
+        "/assets/Habtoor Grand/Club Oceanfront King/04.jpeg",
+        "/assets/Habtoor Grand/Club Oceanfront King/05.jpg",
+        "/assets/Habtoor Grand/Club Oceanfront King/06.jpg",
+        "/assets/Habtoor Grand/Club Oceanfront King/07.jpg",
+        "/assets/Habtoor Grand/Club Oceanfront King/08.jpg",
+        "/assets/Habtoor Grand/Club Oceanfront King/09.jpg",
+        "/assets/Habtoor Grand/Club Oceanfront King/10.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Club Room King",
+      images: [
+        "/assets/Habtoor Grand/Club Room King/03.jpeg",
+        "/assets/Habtoor Grand/Club Room King/01.jpeg",
+        "/assets/Habtoor Grand/Club Room King/02.jpeg",
+        "/assets/Habtoor Grand/Club Room King/04.jpeg",
+        "/assets/Habtoor Grand/Club Room King/05.jpeg",
+        "/assets/Habtoor Grand/Club Room King/06.jpeg",
+        "/assets/Habtoor Grand/Club Room King/07.jpeg",
+        "/assets/Habtoor Grand/Club Room King/08.jpeg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Family Suite",
+      images: [
+        "/assets/Habtoor Grand/Family Suite/01.jpeg",
+        "/assets/Habtoor Grand/Family Suite/02.jpeg",
+        "/assets/Habtoor Grand/Family Suite/03.jpeg",
+        "/assets/Habtoor Grand/Family Suite/04.jpeg",
+        "/assets/Habtoor Grand/Family Suite/05.jpeg",
+        "/assets/Habtoor Grand/Family Suite/06.jpg",
+        "/assets/Habtoor Grand/Family Suite/07.jpg",
+        "/assets/Habtoor Grand/Family Suite/08.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Resort Garden King",
+      images: [
+        "/assets/Habtoor Grand/Resort Garden King/01.jpeg",
+        "/assets/Habtoor Grand/Resort Garden King/02.jpeg",
+        "/assets/Habtoor Grand/Resort Garden King/03.jpeg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Tower Family",
+      images: [
+        "/assets/Habtoor Grand/Tower Family/04.jpeg",
+        "/assets/Habtoor Grand/Tower Family/01.jpeg",
+        "/assets/Habtoor Grand/Tower Family/02.jpeg",
+        "/assets/Habtoor Grand/Tower Family/03.jpeg",
+        "/assets/Habtoor Grand/Tower Family/05.jpeg",
+        "/assets/Habtoor Grand/Tower Family/06.jpg",
+      ],
+      discount: 30,
+    },
+    {
+      name: "Tower Partial Ocean",
+      images: [
+        "/assets/Habtoor Grand/Tower Partial Ocean/06.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/03.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/01.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/02.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/04.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/05.jpeg",
+        "/assets/Habtoor Grand/Tower Partial Ocean/07.webp",
+        "/assets/Habtoor Grand/Tower Partial Ocean/08.webp",
+        "/assets/Habtoor Grand/Tower Partial Ocean/09.avif",
+        "/assets/Habtoor Grand/Tower Partial Ocean/10.jpeg",
+      ],
+      discount: 30,
+    },
+  ],
+  gridImages: [
+    "/assets/Habtoor Grand/Tower Partial Ocean/02.jpeg",
+    "/assets/Habtoor Grand/Club Room King/03.jpeg",
+    "/assets/Habtoor Grand/Club Oceanfront King/02.jpeg",
+  ],
+  subheadline:
+    "In the heat of the of the prestigious Dubai Mariana at the very beginning of the wold famous Jumeirah Beach Strip.",
+  benefits: [
+    "Acaia Lounge",
+    "Al Basha",
+    "Al Dhiyafa Grand Kitchen",
+    "Al Manara",
+    "Luciano’s Set Menu 1",
+    "Polo Bar",
+    "Salamar",
+    "The Underground Pub",
+    "Pool Bar",
+  ],
+  logo: "/assets/habtoor-grand.png",
+  link: "/habtoor-grand",
+};
+
+export const hotels = [
+  "Hilton Al Habtoor City",
+  "Habtoor Palace",
+  "V Hotel",
+  "Habtoor Grand",
+  "The Mirage",
+  "Metropolitan Hotel",
+];
+
+export const hotelsList = [
+  {
+    name: "Hilton Al Habtoor City",
+    benefits: ["Restaurants", "Buffet", "Pool Bar"],
+    image: "/assets/Hilton Al Habtoor City/Executive King/01.jpg",
+    link: "/hilton-al-habtoor-city",
+  },
+  {
+    name: "Habtoor Palace",
+    benefits: ["Beach Club", "Buffet", "Bar"],
+    image: "/assets/Habtoor Palace/Ambassador suite/01.png",
+    link: "/habtoor-palace",
+  },
+  {
+    name: "V Hotel",
+    benefits: ["Pub", "Buffet", "Bar"],
+    image: "/assets/V Hotel/Deluxe/01.jpg",
+    link: "/v-hotel",
+  },
+  {
+    name: "The Mirage",
+    benefits: ["Buffet", "Bar", "Archery"],
+    image: "/assets/The Mirage/Junior Suite/01.jpg",
+    link: "/the-mirage",
+  },
+  {
+    name: "Habtoor Grand",
+    benefits: ["Buffet", "Bar", "Archery"],
+    image: "/assets/Habtoor Grand/Tower Partial Ocean/01.jpeg",
+    link: "/habtoor-grand",
+  },
+  {
+    name: "Metropolitan Hotel",
+    benefits: ["Al Safa", "Tropicana", "Al Sheif"],
+    image: "/assets/Metropolitan Hotel/Deluxe Suite/01.jpg",
+    link: "/metropolitan-hotel",
+  },
+];
+
+function mergeRooms(hotel) {
+  const hotelName = hotel.name;
+  const link = hotel.link;
+  const roomTypes = hotel.room_types.map((roomType) => ({
+    hotelName,
+    link,
+    ...roomType,
+  }));
+  return roomTypes;
+}
+
+function getRandomizedRooms(hotels) {
+  const allRooms = hotels.flatMap((hotel) => mergeRooms(hotel));
+  return allRooms.sort(() => Math.random() - 0.5);
+}
+
+export const allHotels = [
+  hilton,
+  habtoorGrand,
+  habtoorPalace,
+  metropolitanHotel,
+  vHotel,
+  theMirage,
+];
+
+export const roomsList = getRandomizedRooms([
+  habtoorPalace,
+  vHotel,
+  habtoorGrand,
+  metropolitanHotel,
+  hilton,
+  theMirage,
+]);
